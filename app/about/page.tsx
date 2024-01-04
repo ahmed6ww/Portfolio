0aa433735849b3bdf6d3da5414f254f94423771e
Ahmed <heydude08080@gmail.com>
2024-01-05 01:26:10 +0500

    Modified

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const about = () => {
   return (
     <section className=''>
-      <div className='relative h-screen flex flex-row '>
+      <div className='relative h-screen flex flex-row  '>
         <div className='absolute   bottom-0 right-0 flex justify-end  '>
           <Image
             src='/about.png'
@@ -12,32 +12,30 @@ const about = () => {
             height={600}
             width={650}
             alt='Backgrounf Image'
-            className='invisible lg:visible '
+            className='invisible lg:visible'
           />
         </div>
-        <div className=' absolute top-56  '>
-          <h1 className='text-white ml-11  font-bold md:text-3xl lg:text-4xl sm:text-lg '>About</h1>
-          <p className='  sm:text-sm ml-32 flex md:text-lg lg:text-lg  text-white justify-center items-center  sm:mr-[120px] md:mr-[120px] lg:mr-[620px] xl:mr-[720px] mt-10 mr-20 '>Greetings! I&apos;m Ahmed, a passionate Full Stack Web Developer and AI Enthusiast with a flair for creativity. My journey in the vast realm of technology has been an exciting adventure, where I&apos;ve honed my skills to craft seamless and innovative web solutions. With a keen interest in artificial intelligence, I thrive on exploring the intersection of technology and intelligence to create cutting-edge applications.
-            Beyond coding, my love for Graphic Design adds a unique dimension to my skill set. I find joy in turning ideas into visually stunning creations, blending the worlds of aesthetics and functionality. This combination of technical expertise and design sensibility allows me to approach projects with a holistic perspective, ensuring a delightful user experience.
-            As a lifelong learner, I am committed to staying at the forefront of technological advancements, always seeking new challenges to push the boundaries of what is possible. Join me on this journey, where code meets creativity, and let&apos;s build something extraordinary together. Welcome to my digital space - a showcase of my passion for technology, design, and the endless possibilities they offer.</p>
-       
+        <div className=' absolute top-56 '>
+          <h1 className=' ml-11  font-bold md:text-3xl lg:text-4xl sm:text-lg '>About</h1>
+          <p className='  sm:text-sm ml-32 flex md:text-lg lg:text-lg  justify-center  items-center  sm:mr-[120px] md:mr-[120px] lg:mr-[620px] xl:mr-[720px] mt-10 mr-20 '>Hello! I'm Ahmed, a Full Stack Web Developer and AI enthusiast. My expertise lies in crafting seamless web solutions with a touch of creativity. Passionate about the intersection of technology and intelligence, I create cutting-edge applications. My skills extend to Graphic Design, adding a unique dimension to my work. With a holistic approach, I ensure delightful user experiences. A lifelong learner, I stay at the forefront of tech advancements, always seeking new challenges. Join me in building extraordinary projects where code meets creativity. Welcome to my digital space - a showcase of tech, design, and endless possibilities.</p>
 
 
 
 
 
 
-        
-          <h1 className='font-bold text-3xl mt-7 text-white ml-11 '>
+
+
+          <h1 className='font-bold text-3xl mt-7 ml-11 '>
             Languages
           </h1>
-          <div className=' text-white mt-10 flex flex-col gap-3 ml-32'>
+          <div className=' mt-10 flex flex-col gap-3 ml-32'>
             <h4>Urdu</h4>
             <h4>English</h4>
             <h4>German</h4>
             <h4>Punjabi</h4>
-          
- </div>
+
+          </div>
         </div>
 
 
